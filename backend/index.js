fdeb1f7e02b70ab6a872e3e9d30b594f9629a3a2
Manikandan <42240958+ManikandanRJSM@users.noreply.github.com>
2023-03-12 13:37:43 +0530

    adding global error handler middleware into the route
    adding global error handler middleware into the route

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const { errhandler } = require('./middleware/errorhander')
 const port = process.env.PORT
 const app = express()
 //https://www.youtube.com/watch?v=-0exw-9YJBo
 
 app.use('/api/users', require('./routes/user')) // importin the user routes
+
+app.use(errhandler)
 
 app.listen(port, () => {
     console.log(`server running in ${port}`)
