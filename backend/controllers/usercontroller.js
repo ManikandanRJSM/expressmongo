df452fa50f5326f5b8e3ff4ad4b4b46970d32218
Manikandan <42240958+ManikandanRJSM@users.noreply.github.com>
2023-03-12 18:05:00 +0530

    user controller updated
    user controller updated

--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -1,20 +1,29 @@
-const getUsers = (request, response) => {
-    response.status(400).json({message : 'route to get all the users from controllers'})
-}
+const ayncHandeler = require('express-async-handler')
+const bcrypt = require('bcrypt')
+const adminModel   = require('../models/adminModel')
+
+const getUsers = ayncHandeler( async (request, response) => {
+    const admin = await adminModel.findOne({id : request.adminAuth.id})
+    response.status(200).json({data : admin})
+})
 
 
 const registerUser = (request, response) => {
-    response.status(400).json({message : `register the user from controllers`})
+    if(!request.body.name){
+        response.status(400)
+        throw new Error (`name field is required`)
+    } 
+    response.status(200).json({message : `register the user from controllers`})
 }
 
 
 const updateUser = (request, response) => {
-    response.status(400).json({message : `update the user ${request.params.id} from controllers`})
+    response.status(200).json({message : `update the user ${request.params.id} from controllers`})
 }
 
 
 const deleteUser = (request, response) => {
-    response.status(400).json({message : `delete the user ${request.params.id} from controllers`})
+    response.status(200).json({message : `delete the user ${request.params.id} from controllers`})
 }
 
 module.exports = {
