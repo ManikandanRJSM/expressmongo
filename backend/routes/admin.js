9a85f23a52ebe1aa4778f2299743a2e02ffb29d6
Manikandan <42240958+ManikandanRJSM@users.noreply.github.com>
2023-03-12 23:22:52 +0530

    update admin
    update admin

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,19 +1,21 @@
-const express = require('express')
-const route = express.Router()
-const { login, loggedAdmin } = require('../controllers/admincontroller');
+const express                   = require('express')
+const route                     = express.Router()
+const { login, loggedAdmin }    = require('../controllers/admincontroller');
 const { getUsers, registerUser, updateUser, deleteUser } = require('../controllers/usercontroller');
-const { jwtAuth } = require('../middleware/adminAuth');
+const { jwtAuth }               = require('../middleware/adminAuth');
 
 
 route.post('/login', login)
 
 route.get('/get_admin', loggedAdmin)
 
+route.get('/get_users', jwtAuth, getUsers)
+
 route.post('/register_user', jwtAuth, registerUser)
 
-route.put('/update_user', jwtAuth, updateUser)
+route.put('/update_user/:id', jwtAuth, updateUser)
 
-route.put('/delete_user', jwtAuth, deleteUser)
+route.delete('/delete_user/:id', jwtAuth, deleteUser)
 
 
 module.exports = route
