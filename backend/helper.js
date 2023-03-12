dcdffb79887a3bfaf132f2cff8e94554087fff83
Manikandan <42240958+ManikandanRJSM@users.noreply.github.com>
2023-03-12 23:24:15 +0530

    update helper
    update helper

--- backend/helper.js
+++ backend/helper.js
@@ -1,4 +1,8 @@
-const jwt = require('jsonwebtoken')
+const jwt     = require('jsonwebtoken')
+const ayncHandeler      = require('express-async-handler')
+const bcrypt  = require('bcrypt')
+
+
 const generateJWT = (payload, user_type = null) => {
     if (user_type) {
         return jwt.sign({ id: payload, user_type : user_type }, process.env.JWT_SECRET, {
@@ -12,4 +16,12 @@ const generateJWT = (payload, user_type = null) => {
     
 }
 
-module.exports = { generateJWT }
+
+const generateHash = ayncHandeler( async (password) => {
+    
+    const salt      = await bcrypt.genSalt(10)
+    return await bcrypt.hash(password, salt)
+    
+})
+
+module.exports = { generateJWT, generateHash }
