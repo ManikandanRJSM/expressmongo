79712d25dd20f03604c9428f8a4efabfbc284621
Manikandan <42240958+ManikandanRJSM@users.noreply.github.com>
2023-03-12 23:25:08 +0530

    update index js
    update index js

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const { errhandler } = require('./middleware/errorhander')
+const { connectDB } = require('./config/db')
 const port = process.env.PORT
+
+connectDB()
+
 const app = express()
 
-app.use('/api/users', require('./routes/user')) // importin the user routes
+app.use(express.json()); // used to fetch request in the form of json
+app.use(express.urlencoded({ extended : true })) // used to fetch request in the form of urlencode
+
+app.use('/api/users', require('./routes/user')) // importing the user routes
+
+app.use('/api/admin', require('./routes/admin')) // importing the user routes
 
 app.use(errhandler)
 
