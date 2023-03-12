63c6d0892f8a89e88fcc2aef408a4a342f114faf
Manikandan <42240958+ManikandanRJSM@users.noreply.github.com>
2023-03-12 13:38:06 +0530

    update index.js
    update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,6 @@ const dotenv = require('dotenv').config()
 const { errhandler } = require('./middleware/errorhander')
 const port = process.env.PORT
 const app = express()
-//https://www.youtube.com/watch?v=-0exw-9YJBo
 
 app.use('/api/users', require('./routes/user')) // importin the user routes
 
